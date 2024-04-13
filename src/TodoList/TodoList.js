5f4b3301c43ed6284232ec080261125c5b31097c
Kishangaur24 <114826113+Kishangaur24@users.noreply.github.com>
2024-04-13 12:32:32 +0530

    add and delete function has done

--- src/TodoList/TodoList.js
+++ src/TodoList/TodoList.js
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const TodoList = () => {
+    const[todo, setTodo] = useState("")
+    const[newTodo, setNewTodo] = useState([])
+
+    function handleTodo(){
+        setTodo("")
+      setNewTodo([...newTodo,todo])
+    }
+
+    function handleDelete(i){
+        const UpdatedTodos = newTodo.filter((elem, index) => index !== i);
+        setNewTodo(UpdatedTodos);
+    }
+
   return (
     <div>
-      Todo list
+     <h1>Todo - List</h1>
+     <input placeholder='Add Todo'
+      onChange={(e)=>setTodo(e.target.value)} 
+      value={todo}/>
+     <button onClick={handleTodo}>Add</button>
+     <ol>
+        {newTodo.map((item, i) => (
+          <div key={i}>
+            {
+           
+            <ol>{item} 
+            <button onClick={() => handleDelete(i)}>  delete</button>
+            </ol>
+            
+            
+}
+    
+
+            
+          </div>
+        ))}
+      </ol>
     </div>
   )
 }
